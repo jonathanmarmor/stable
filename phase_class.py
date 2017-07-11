a5f946df37f88c7886993aa6c765dbb484125ea0
Jonathan Marmor <jmarmor@gmail.com>
2017-07-10 21:30:26 -0400

    Tidy up the Phase class. Mute all but one track for the first instance.

--- phase_class.py
+++ phase_class.py
@@ -4,7 +4,7 @@ import sox
 
 
 class Sample(object):
-    def __init__(self, file_path):
+    def __init__(self, file_name='master_sample.wav'):
         self.file_name = file_name
         self.full_duration = sox.file_info.duration(file_name)
         self.phrase_duration = 4.207
@@ -13,33 +13,26 @@ class Sample(object):
 
 
 class Phase(object):
-    def __init__(self, sample_file_name, output_file_name, gap=0.5, repeat_count=5):
+    def __init__(self, sample_file_name='master_sample.wav', output_file_name='output.wav'):
         self.sample = Sample(sample_file_name)
         self.output_file_name = output_file_name
-        self.gap = gap
-        self.repeat_count = repeat_count
         self.temp_folder = 'tmp/'
 
     def make_track(self, output_file_name, gap, repeat_count, has_initial_rest=False, mute_first=False):
         rest_duration = self.sample.full_duration + gap - self.sample.start_pad_duration - self.sample.end_pad_duration
 
-        # if mute_first:
-        #     repeat_count -= 1
+        if mute_first:
+            repeat_count -= 1
 
         tfm = sox.Transformer()
         tfm.pad(end_duration=rest_duration)
         if repeat_count > 0:
             tfm.repeat(count=repeat_count)
-        # if mute_first:
-            # TODO
-
-
-
-
-
         if has_initial_rest:
             tfm.pad(start_duration=rest_duration + ((self.sample.full_duration - rest_duration) / 2.0))
-        tfm.build(sample.file_name, output_file_name)
+        if mute_first:
+            tfm.pad(start_duration=self.sample.full_duration + rest_duration)
+        tfm.build(self.sample.file_name, output_file_name)
 
     def checker_track(self, output_file_name, gap=1.0, repeat_count=5, mute_first=False):
         """Repeat the sample on alternating tracks so the fade in and out can overlap"""
@@ -53,36 +46,43 @@ class Phase(object):
         self.make_track(track_b_file, gap, track_b_repeat_count, has_initial_rest=True)
 
         cbn = sox.Combiner()
-        # cbn.silence(location=1)
-        # cbn.silence(location=-1)
+        cbn.silence(location=-1)
         cbn.build([track_a_file, track_b_file], output_file_name, 'mix-power')
 
-    def go(self):
+    def go(self, n_tracks, gap, repeat_count):
         track_file_names = []
-        for i in range(1, 4):
+        for i in range(1, n_tracks):
             track_file_name = self.temp_folder + 'track-{}.wav'.format(i)
             track_file_names.append(track_file_name)
             mute_first = False
-            if i == 1:
+            if i is not 1:
                 mute_first = True
-            self.checker_track(track_file_name, gap=self.gap * i, repeat_count=self.repeat_count, mute_first=mute_first)
+            self.checker_track(track_file_name, gap=gap * i, repeat_count=repeat_count, mute_first=mute_first)
 
         cbn = sox.Combiner()
         cbn.build(track_file_names, self.output_file_name, 'mix-power')
 
 
 if __name__ == '__main__':
-    # import argparse
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument(
-    #     '-n',
-    #     '--n-tracks',
-    #     help='how many tracks to generate')
-    # args = parser.parse_args()
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-n',
+        '--n-tracks',
+        help='how many tracks to generate',
+        type=int)
+    parser.add_argument(
+        '-g',
+        '--gap',
+        help='the smallest gap between phrases',
+        type=float)
+    parser.add_argument(
+        '-r',
+        '--repeat-count',
+        help='the number of times the phrase should repeat',
+        type=int)
 
-    # simple_phase(args.n_tracks)
-    sample_file_name = 'master_sample.wav'
-    output_file_name = 'output.wav'
-    gap = 0.5
-    repeat_count = 5
-    Phase(sample_file_name, output_file_name, gap, repeat_count)
+    args = parser.parse_args()
+
+    phase = Phase()
+    phase.go(args.n_tracks, args.gap, args.repeat_count)
